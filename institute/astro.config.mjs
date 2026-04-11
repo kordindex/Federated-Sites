94a61812eca1def93d84587c258cb6a0f558c8c6
kordindex <kord@hellosignal.com>
2026-04-10 21:29:12 -0600

    Nav Updates

--- institute/astro.config.mjs
+++ institute/astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-// https://astro.build/config
-export default defineConfig({});
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@shared': path.resolve(__dirname, '../shared'),
+      },
+    },
+  },
+});
